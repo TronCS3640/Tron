0d320429afb044b4e34222c96be4916ef27450bb
Nicholas Polach <npolach@csub.edu>
2017-10-24 18:31:45 -0700

    Added support for multiple players and collision

--- tron.py
+++ tron.py
@@ -17,6 +17,10 @@ RED = (255, 0, 0)
 LIGHTRED = (255, 125, 125)
 BLUE = (0, 0, 255)
 LIGHTBLUE = (125, 125, 255)
+GREEN = (0, 255, 0)
+LIGHTGREEN = (125, 255, 125)
+PURPLE = (255, 0, 255)
+LIGHTPURPLE = (255, 125, 255)
 BLACK = (0, 0, 0)
 
 # Use single buffer
@@ -30,11 +34,14 @@ class TronWindow(pyglet.window.Window):
                                          config=config)
         self.running = True
 
-        self.player1 = player.Player(0, 0)
+        self.players = [player.Player(int(BOARDWIDTH/4),   int(BOARDHEIGHT/4)),
+                        player.Player(int(BOARDWIDTH/4*3), int(BOARDHEIGHT/4))]
+#                        player.Player(int(BOARDWIDTH/4),   int(BOARDHEIGHT/4*3)),
+#                        player.Player(int(BOARDWIDTH/4*3), int(BOARDHEIGHT/4*3))]
 
-        self.movement = "up"
+        self.movement1 = "up"
+        self.movement2 = "up"
 
-        #pyglet.clock.schedule_interval(self.update, .5)
         pyglet.clock.schedule_interval(self.update, .03)
 
         #pyglet.clock.set_fps_limit(60)
@@ -46,14 +53,58 @@ class TronWindow(pyglet.window.Window):
         if not self.running:
             exit()
 
-        if self.movement == "up":
-            self.player1.move_ip(0, 1)
-        elif self.movement == "left":
-            self.player1.move_ip(-1, 0)
-        elif self.movement == "right":
-            self.player1.move_ip(1, 0)
-        elif self.movement == "down":
-            self.player1.move_ip(0, -1)
+        # Player 1
+        # WASD
+        if self.movement1 == "up":
+            self.players[0].move_ip(0, 1)
+        elif self.movement1 == "left":
+            self.players[0].move_ip(-1, 0)
+        elif self.movement1 == "right":
+            self.players[0].move_ip(1, 0)
+        elif self.movement1 == "down":
+            self.players[0].move_ip(0, -1)
+
+        # Player 2
+        # Arrow keys
+        if self.movement2 == "up":
+            self.players[1].move_ip(0, 1)
+        elif self.movement2 == "left":
+            self.players[1].move_ip(-1, 0)
+        elif self.movement2 == "right":
+            self.players[1].move_ip(1, 0)
+        elif self.movement2 == "down":
+            self.players[1].move_ip(0, -1)
+
+#        # Player 3
+#        # WASD
+#        if self.movement1 == "up":
+#            self.players[2].move_ip(0, 1)
+#        elif self.movement1 == "left":
+#            self.players[2].move_ip(-1, 0)
+#        elif self.movement1 == "right":
+#            self.players[2].move_ip(1, 0)
+#        elif self.movement1 == "down":
+#            self.players[2].move_ip(0, -1)
+
+#        # Player 4
+#        # Arrow keys
+#        if self.movement2 == "up":
+#            self.players[3].move_ip(0, 1)
+#        elif self.movement2 == "left":
+#            self.players[3].move_ip(-1, 0)
+#        elif self.movement2 == "right":
+#            self.players[3].move_ip(1, 0)
+#        elif self.movement2 == "down":
+#            self.players[3].move_ip(0, -1)
+
+    def check_players_collide(self):
+
+        for pnum in range(len(self.players)):
+            curpos = tuple(self.players[pnum].pos)
+            for pnum2 in range(len(self.players)):
+                if curpos in self.players[pnum2].trail:
+                    print("Collides")
+                    return True
 
     def create_quad_vertex_list(self, x, y):
         return (x*CELLSIZE,            y*CELLSIZE,
@@ -66,13 +117,22 @@ class TronWindow(pyglet.window.Window):
 
     def on_key_press(self, symbol, modifiers):
         if symbol == key.W:
-            self.movement = "up"
+            self.movement1 = "up"
         elif symbol == key.A:
-            self.movement = "left"
+            self.movement1 = "left"
         elif symbol == key.D:
-            self.movement = "right"
+            self.movement1 = "right"
         elif symbol == key.S:
-            self.movement = "down"
+            self.movement1 = "down"
+
+        elif symbol == key.UP:
+            self.movement2 = "up"
+        elif symbol == key.LEFT:
+            self.movement2 = "left"
+        elif symbol == key.RIGHT:
+            self.movement2 = "right"
+        elif symbol == key.DOWN:
+            self.movement2 = "down"
         elif symbol == key.ESCAPE:
             self.running = False
 
@@ -80,27 +140,43 @@ class TronWindow(pyglet.window.Window):
         # Save time by drawing black quad over removed trail cells
         # This way all trail cells do not need to be drawn every step
 
-        # Example player
-        vertex_list = pyglet.graphics.vertex_list(4, ('v2i', self.create_quad_vertex_list(
-                                                        self.player1.pos[0],
-                                                        self.player1.pos[1])),
-                                                     ('c3B', self.create_quad_color_list(RED)))
-        vertex_list.draw(pyglet.gl.GL_QUADS)
+        for pnum in range(len(self.players)):
 
-        # Example trail
-        if len(self.player1.trail) > 0:
-            if len(self.player1.trail) > 250:
-                del_x, del_y = self.player1.trail.pop(0)
-                trail_list = pyglet.graphics.vertex_list(4, ('v2i', self.create_quad_vertex_list(del_x, del_y)),
-                                                                ('c3B', self.create_quad_color_list(BLACK)))
+            if pnum==0:
+                color1=RED
+                color2=LIGHTRED
+            elif pnum==1:
+                color1=BLUE
+                color2=LIGHTBLUE
+            elif pnum==2:
+                color1=GREEN
+                color2=LIGHTGREEN
+            elif pnum==3:
+                color1=PURPLE
+                color2=LIGHTPURPLE
+
+            # Draw player
+            vertex_list = pyglet.graphics.vertex_list(4, ('v2i', self.create_quad_vertex_list(
+                                                            self.players[pnum].pos[0],
+                                                            self.players[pnum].pos[1])),
+                                                        ('c3B', self.create_quad_color_list(color1)))
+            vertex_list.draw(pyglet.gl.GL_QUADS)
+
+            # Draw trail
+            if len(self.players[pnum].trail) > 0:
+                if len(self.players[pnum].trail) > 250:
+                    del_x, del_y = self.players[pnum].trail.pop(0)
+                    trail_list = pyglet.graphics.vertex_list(4, ('v2i', self.create_quad_vertex_list(del_x, del_y)),
+                                                                    ('c3B', self.create_quad_color_list(BLACK)))
+                    trail_list.draw(pyglet.gl.GL_QUADS)
+                new_x = self.players[pnum].trail[-1][0]
+                new_y = self.players[pnum].trail[-1][1]
+                trail_list = pyglet.graphics.vertex_list(4, ('v2i', self.create_quad_vertex_list(new_x, new_y)),
+                                                                ('c3B', self.create_quad_color_list(color2)))
                 trail_list.draw(pyglet.gl.GL_QUADS)
-            new_x = self.player1.trail[-1][0]
-            new_y = self.player1.trail[-1][1]
-            trail_list = pyglet.graphics.vertex_list(4, ('v2i', self.create_quad_vertex_list(new_x, new_y)),
-                                                            ('c3B', self.create_quad_color_list(LIGHTRED)))
-            trail_list.draw(pyglet.gl.GL_QUADS)
 
-        #self.label.draw()
+        if self.check_players_collide():
+            exit()
 
 if __name__ == "__main__":
 
